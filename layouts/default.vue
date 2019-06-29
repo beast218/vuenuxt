7992b16bafa7441564aa91684ce1bd075df7df8f
[y1-aaron@ik8s.com] <y1-aaron@ik8s.com>
2019-06-29 16:22:27 +0800

    111

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>This is Default.Vue</h1>
+  </div>
+</template>
+
+<style lang="less">
+  @import "../assets/styles/index";
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+const resolve = require('path').resolve
+const pkg = require('./package')
+require('dotenv').config()
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: resolve(__dirname, 'components/loading.vue'),
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    // '@/assets/styles/common/normalize.less',
+    // '@/assets/styles/common/reset.less',
+    // '@/assets/styles/index.less'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    analyze: false,
+    publicPath: (process.env.CDN_PATH ? process.env.CDN_PATH : '') + '/.nuxt/dist/',
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
